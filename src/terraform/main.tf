7af87cad432e94dfc93f9c7521e899531f9c5f3e
unknown <anjaleeirudinee9396@gmail.com>
2024-03-17 14:15:30 +0530

    added plan

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,14 +1,4 @@
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
-    }
-  }
-
-  
-}
 
 
 resource "azurerm_resource_group" "devops" {
