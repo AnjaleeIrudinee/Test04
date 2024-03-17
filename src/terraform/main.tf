3959baf4565d96b9486df58f9aec01e6c0fcf703
unknown <anjaleeirudinee9396@gmail.com>
2024-03-17 14:30:54 +0530

    added plan

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,11 +1,18 @@
-
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0"
     }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "devops"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "demo.terraform.tfstate"
+   
   }
 }
 
