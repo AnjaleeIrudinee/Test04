538756dc512de49fecd63b989eedc9cc56face37
unknown <anjaleeirudinee9396@gmail.com>
2024-03-17 13:43:49 +0530

    added storage account

---
+++ src/terraform/main.tf
@@ -0,0 +1,36 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "devops"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "demo.terraform.tfstate"
+   
+  }
+}
+ tags = {
+    owner = "anjalee"
+  }
+provider "azurerm" {
+  features {}
+ 
+}
+resource "azurerm_storage_account" "storagetest01" {
+  name                     = "storageaccounttest"
+  resource_group_name      = azurerm_resource_group.devops.name
+  location                 = azurerm_resource_group.devops.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Learning"
+  }
+}
